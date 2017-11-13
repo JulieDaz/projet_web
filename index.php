9b411d53fde9a3babf5ad4ccad826ca253aa7167
Yu-jinKim <yujin.kim@hotmail.fr>
2017-11-13 15:28:56 +0100

    Mise en page

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 <form metohd="post" action="traitement.php">
   <label>ID</label> : <input type="text" name="ID">
+  <br><br>
   <label>Mot de passe</label> : <input type="password" name="Mdp">
+  <br><br>
   <input type="submit" value="Se connecter">
 </form>
 
