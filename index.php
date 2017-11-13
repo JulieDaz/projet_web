d9c6a0a51395fb5a92764a0b32261ea1841e643f
Yu-jinKim <yujin.kim@hotmail.fr>
2017-11-13 15:08:46 +0100

    Page d'accueil

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <h1>Page d'accueil</h1>
 
 <form metohd="post" action="fichier.php">
-  
+  <label>ID</label> : <input type="text" name="ID">
+  <label>Mot de passe</label> : <input type="text" name="Mdp">
 </form>
