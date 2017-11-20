ce7225c43ed28de2eda602d49e807e724e20059a
JulieDaz <julie.dazeniere@gmail.com>
2017-11-20 14:32:05 +0100

    J : ajout fich fonction.php / traitement.php et mod index.php

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
-<h1>Page d'accueil</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 
-<form metohd="post" action="traitement.php">
+<h1>Welcome to Medical Planner</h1>
+
+<form method="post" action="traitement.php">
   <label>ID</label> : <input type="text" name="ID">
   <br><br>
   <label>Mot de passe</label> : <input type="password" name="Mdp">
   <br><br>
   <input type="submit" value="Se connecter">
 </form>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
 
---
+++ traitement.php
@@ -0,0 +1,11 @@
+<?php include("fonction.php");
+
+$connexion = connect() ;
+$id = $_POST['ID'] ;
+$req = 'SELECT' . $id . 'FROM medecin'
+echo "coucou ça marche"
+
+
+
+
+?>
