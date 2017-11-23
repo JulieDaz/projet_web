80a28ed9ef88c7abb1c6c5b497b2f1816a98e3b3
Yu-jinKim <yujin.kim@hotmail.fr>
2017-11-23 22:19:35 +0100

    Y: Ajout d'un début du fonction pour récupérer les créneaux

--- traitement.php
+++ traitement.php
@@ -13,7 +13,7 @@ else {
 	$user="Medecin" ;
 	$identifiant = "IDm" ;
 	$login = "SELECT * FROM $user WHERE $identifiant = '$id' AND Mdp = '$mdp'" ;
-	$verif = do_request($login, $connexion);
+	$verif = do_request($connexion,$login);
 	if ($verif == TRUE) {
 		print("Connexion réussie : vous êtes un médecin");
 		}
@@ -21,7 +21,7 @@ else {
 			$user="Responsable_d_intervention" ;
 			$identifiant = "IDr" ;
 			$login = "SELECT * FROM $user WHERE $identifiant = '$id' AND Mdp = '$mdp'" ;
-			$verif2 = do_request($login, $connexion);
+			$verif2 = do_request($connexion, $login);
 			if ($verif2 == TRUE) {
 				print("Connexion réussie : vous êtes un responsable");
 				}
@@ -29,15 +29,54 @@ else {
 				$user="Administrateur" ;
 				$identifiant = "IDa" ;
 				$login = "SELECT * FROM $user WHERE $identifiant = '$id' AND Mdp = '$mdp'" ;
-				$verif3 = do_request($login, $connexion);
+				$verif3 = do_request($connexion,$login);
 				if ($verif3 == TRUE) {
 					print("Connexion réussie : vous êtes un admin");
 					}
 				else {	
-					print("va te faire foutre enculé de ta mère") ;
+					print("Veuillez vérifier votre identifiant ou votre mot de passe") ;
 				}	
 			}
 		}
 	}
 
-//print($user);
+$creneaux = get_creneaux($user,$id,$connexion);
+
+print_creneaux($creneaux);
+
+?>
+
+<head>
+    <title>Medical Planner</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+	<br>
+	<table>
+	<?php
+		$jour = array(null, "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
+		$rdv["Dimanche"]["16:30"] = "Dermatologue";
+		$rdv["Dimanche"]["17"] = "Dermatologue";
+		$rdv["Lundi"]["9"] = "Mémé -_-";
+		echo "<tr><th>Heure</th>";
+		for($x = 1; $x < 8; $x++)
+			echo "<th>".$jour[$x]."</th>";
+		echo "</tr>";
+		for($j = 8; $j < 20; $j += 0.5) {
+			echo "<tr>";
+			for($i = 0; $i < 7; $i++) {
+				if($i == 0) {
+					$heure = str_replace(".5", ":30", $j);
+					echo "<td class=\"time\">".$heure."</td>";
+				}
+				echo "<td>";
+				if(isset($rdv[$jour[$i+1]][$heure])) {
+					echo $rdv[$jour[$i+1]][$heure];
+				}
+				echo "</td>";
+			}
+			echo "</tr>";
+		}
+	?>
+	</table>
+</body>
