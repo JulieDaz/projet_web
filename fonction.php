754b3824e48b772fcfdab92afb1412aa8a9edad2
Yu-jinKim <yujin.kim@hotmail.fr>
2017-11-23 22:18:43 +0100

    Y: Ajout du code html pour afficher un emploi du temps

--- fonction.php
+++ fonction.php
@@ -1,6 +1,6 @@
 <?php  
 
-/***** Connexion à la base de donnée ******/
+/* Connexion à la base de donnée */
 function connect()
 {
     $user = 'root'; // utilisatrice
@@ -18,17 +18,16 @@ function connect()
     return $connexion;
 }
 
-/*****deconnexion a la base de donnée****/
+/* Déconnexion a la base de données */
 
 function deconnect($connexion)
 {
 	mysqli_close($connexion);
 }
 
-function do_request($request,$connexion)
+function do_request($connexion,$request)
 {
-    $req = mysqli_query($connexion,$request) or die('Erreur SQL !<br>'.$request.'<br>'.mysqli_error());
-    $a = array();
+    $req = mysqli_query($connexion,$request) or die('<br>Erreur SQL !<br>'.$request.'<br>'.mysqli_error($connexion));
     while($row = mysqli_fetch_assoc($req))
     {
         $a[] = $row;
@@ -37,7 +36,33 @@ function do_request($request,$connexion)
     return $a;
 }
 
-function print_request($array) // en cours de réalisation
+/* Récupérer les créneaux à afficher selon le type d'utilisateur */
+function get_creneaux($job, $ID, $connexion)
+{
+    if ($job == "Medecin") // s'il s'agit d'un médecin
+    {
+        $request_IDp = "SELECT IDp FROM a_comme WHERE IDm='$ID'"; // première requête pour récupérer les ID patients du médecin considéré
+        $IDp_from_medecin = do_request($connexion,$request_IDp); // on effectue la requête --> tableau de tableau
+        foreach($IDp_from_medecin as $IDp_array) // pour chaque ID patient récupéré
+        {
+            $IDp = $IDp_array['IDp']; // on va chercher la valeur contenue par la clé 'IDp'
+            $request_IDc = "SELECT IDc FROM patient WHERE IDp = $IDp"; // deuxième requête pour récupérer les ID créneaux pour chaque patient
+            $IDc_from_patient = do_request($connexion, $request_IDc); // on effectue la requête --> tableau de tableau
+        }
+        foreach($IDc_from_patient as $IDc_array) // pour chaque créneau récupéré
+        {
+            $IDc = $IDc_array['IDc']; // on va chercher la valeur contenue par la clé 'IDc'
+            $request_HDebut = "SELECT DATE_FORMAT(Heure_debut, '%d/%m/%y %H:%i:%s') as Heure_debut FROM planning WHERE IDc = $IDc"; // requête pour récupérer l'heure de début du créneau
+            $request_HFin = "SELECT DATE_FORMAT(Heure_fin, '%d/%m/%y %H:%i:%s') as Heure_fin FROM planning WHERE IDc = $IDc"; // requête pour récupérer l'heure de fin du créneau
+            $Heure_debut = do_request($connexion, $request_HDebut); // on effectue la requête
+            $Heure_fin = do_request($connexion, $request_HFin); // on effectue la requête
+        }
+        return array($Heure_debut,$Heure_fin); // on retourne un tableau de 2 tableaux dans lequel le premier contient les heures de début des créneaux et le deuxième contient les heures de fin
+    }
+    // if ($job == "")
+}
+
+function print_request($array)
 {
     for($i=0;$i<sizeof($array);$i++)
     {
@@ -45,6 +70,27 @@ function print_request($array) // en cours de réalisation
         foreach($row as $data)
         {
             print($data);
+            print("<br>");
+        }
+    }
+}
+
+function print_creneaux($array)
+{
+    // $array
+    for($i=0;$i<sizeof($array);$i++)
+    {
+        $limites = $array[$i];
+        foreach($limites as $limite)
+        {
+            if (array_key_exists('Heure_debut',$limite))
+            {
+                print($limite['Heure_debut']);
+            }
+            else
+            {
+                print($limite['Heure_fin']);
+            }
             print("<br>");
         }
     }
