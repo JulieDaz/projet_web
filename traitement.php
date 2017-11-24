36e7e7df0ae8280cf4d2079f9c6e9c1aec91ab70
JulieDaz <julie.dazeniere@gmail.com>
2017-11-24 08:51:32 +0100

    J : mod fct authentification

--- traitement.php
+++ traitement.php
@@ -6,77 +6,49 @@ $mdp = $_POST['mdp'] ;
 
 print("<br>") ; 
 
-if ($id == "" OR $mdp == "") {
+if ($id == "" OR $mdp == ""){
 	print("Attention vous n'avez pas rempli tous les champs. Veuillez recommencer.");
-}
+ }
 else {
-	$user="Medecin" ;
-	$identifiant = "IDm" ;
-	$login = "SELECT * FROM $user WHERE $identifiant = '$id' AND Mdp = '$mdp'" ;
-	$verif = do_request($connexion,$login);
-	if ($verif == TRUE) {
-		print("Connexion réussie : vous êtes un médecin");
-		}
-		elseif ($verif == FALSE) {
-			$user="Responsable_d_intervention" ;
-			$identifiant = "IDr" ;
-			$login = "SELECT * FROM $user WHERE $identifiant = '$id' AND Mdp = '$mdp'" ;
-			$verif2 = do_request($connexion, $login);
-			if ($verif2 == TRUE) {
-				print("Connexion réussie : vous êtes un responsable");
-				}
-			elseif ($verif2 == FALSE) {
-				$user="Administrateur" ;
-				$identifiant = "IDa" ;
-				$login = "SELECT * FROM $user WHERE $identifiant = '$id' AND Mdp = '$mdp'" ;
-				$verif3 = do_request($connexion,$login);
-				if ($verif3 == TRUE) {
-					print("Connexion réussie : vous êtes un admin");
-					}
-				else {	
-					print("Veuillez vérifier votre identifiant ou votre mot de passe") ;
-				}	
-			}
-		}
-	}
+	$allusers= "SELECT 'UserType' FROM Utilisateurs WHERE ID ='".$id."' AND Mdp = '".$mdp."'" ;
+	$getallusers = do_request($getallusers,$connexion) ;
+	$getallusers = mysqli_fetch_array($getallusers) ;
+	$getuser = $getallusers[0] ;
+ }
 
-$creneaux = get_creneaux($user,$id,$connexion);
 
-print_creneaux($creneaux);
+// if ($id == "" OR $mdp == "") {
+// 	print("Attention vous n'avez pas rempli tous les champs. Veuillez recommencer.");
+// }
+// else {
+// 	$user="Medecin" ;
+// 	$identifiant = "IDm" ;
+// 	$login = "SELECT * FROM $user WHERE $identifiant = '$id' AND Mdp = '$mdp'" ;
+// 	$verif = do_request($login, $connexion);
+// 	if ($verif == TRUE) {
+// 		print("Connexion réussie : vous êtes un médecin");
+// 		}
+// 	elseif ($verif == FALSE) {
+// 		$user="Responsable_d_intervention" ;
+// 		$identifiant = "IDr" ;
+// 		$login = "SELECT * FROM $user WHERE $identifiant = '$id' AND Mdp = '$mdp'" ;
+// 		$verif2 = do_request($login, $connexion);
+// 		if ($verif2 == TRUE) {
+// 			print("Connexion réussie : vous êtes un responsable");
+// 				}
+// 		elseif ($verif2 == FALSE) {
+// 				$user="Administrateur" ;
+// 				$identifiant = "IDa" ;
+// 				$login = "SELECT * FROM $user WHERE $identifiant = '$id' AND Mdp = '$mdp'" ;
+// 				$verif3 = do_request($login, $connexion);
+// 				if ($verif3 == TRUE) {
+// 					print("Connexion réussie : vous êtes un admin");
+// 					}
+// 				else {	
+// 					print("va te faire foutre enculé de ta mère") ;
+// 				}	
+// 			}
+// 		}
+// 	}
 
-?>
-
-<head>
-    <title>Medical Planner</title>
-    <link rel="stylesheet" href="style.css"/>
-</head>
-<body>
-	<br>
-	<table>
-	<?php
-		$jour = array(null, "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
-		$rdv["Dimanche"]["16:30"] = "Dermatologue";
-		$rdv["Dimanche"]["17"] = "Dermatologue";
-		$rdv["Lundi"]["9"] = "Mémé -_-";
-		echo "<tr><th>Heure</th>";
-		for($x = 1; $x < 8; $x++)
-			echo "<th>".$jour[$x]."</th>";
-		echo "</tr>";
-		for($j = 8; $j < 20; $j += 0.5) {
-			echo "<tr>";
-			for($i = 0; $i < 7; $i++) {
-				if($i == 0) {
-					$heure = str_replace(".5", ":30", $j);
-					echo "<td class=\"time\">".$heure."</td>";
-				}
-				echo "<td>";
-				if(isset($rdv[$jour[$i+1]][$heure])) {
-					echo $rdv[$jour[$i+1]][$heure];
-				}
-				echo "</td>";
-			}
-			echo "</tr>";
-		}
-	?>
-	</table>
-</body>
+//print($user);
