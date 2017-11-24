0bf5c96cb331d3812465950f9a098eacd26a84f8
JulieDaz <julie.dazeniere@gmail.com>
2017-11-24 17:26:56 +0100

    J : création futurs fichiers des formulaires d'ajout/retrait

---
+++ ajouter.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title>Formulaire 1</title>
+</head>
+<body>
+
+<div id="medecin">
+
+    <form method="post" action=""> 
+        <label>Nom</label> : <input type="text" required="on" name="ID">
+        <br><br>
+        <label>Prénom</label> : <input type="text" required="on" name="ID">
+        <br><br>
+        <label>Service d'accueil</label> : <select >
+        <input type="submit" value="Se connecter">
+	</form>
+
+
+
+</div>
+
+
+
+
+</body>
+</html>
