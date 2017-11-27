9d86d2b553c174b08aaa06b0f8b8467484054aa9
Yu-jinKim <yujin.kim@hotmail.fr>
2017-11-27 23:56:03 +0100

    Y: Ajustement de la fonction get_creneaux pour récupérer toutes les données sur plusieurs rdv, déplacement de la fonction get_dates_semaines qui était dans le fichier traitement.php, correction de nom_jour

--- fonction.php
+++ fonction.php
@@ -51,23 +51,26 @@ function get_creneaux($job, $ID, $connexion)
             $request_nom = "SELECT Nom FROM patient WHERE IDp ='$IDp'"; // requête pour récupérer les noms des patients
             $request_prenom = "SELECT Prenom FROM patient WHERE IDp ='$IDp'"; // requête pour récupérer les prénoms des patients
             $request_IDc = "SELECT IDc FROM creneaux WHERE IDp = '$IDp'"; // requête pour récupérer les ID créneaux pour chaque patient
-            $Nom = do_request($connexion, $request_nom);
-            $Prenom = do_request($connexion, $request_prenom);
-            $IDc_arrays = do_request($connexion, $request_IDc);
+            $Nom[] = do_request($connexion, $request_nom);
+            $Prenom[] = do_request($connexion, $request_prenom);
+            $IDc_super_array[] = do_request($connexion, $request_IDc);
         }
-        foreach($IDc_arrays as $IDc_array) // pour chaque créneau récupéré
+        foreach($IDc_super_array as $IDc_arrays) // pour chaque créneau récupéré
         {
+            foreach($IDc_arrays as $IDc_array)
+            {
             $IDc = $IDc_array['IDc']; // on va chercher la valeur contenue par la clé 'IDc'
             $request_HDebut = "SELECT Heure_debut FROM creneaux WHERE IDc = '$IDc'"; // requête pour récupérer l'heure de début du créneau
             $request_HFin = "SELECT Heure_fin FROM creneaux WHERE IDc = '$IDc'"; // requête pour récupérer l'heure de fin du créneau
-            $Heure_debut = do_request($connexion, $request_HDebut); // on effectue la requête
-            $Heure_fin = do_request($connexion, $request_HFin); // on effectue la requête
+            $Heure_debut[] = do_request($connexion, $request_HDebut); // on effectue la requête
+            $Heure_fin[] = do_request($connexion, $request_HFin); // on effectue la requête
+            }
         }
-        return array($Heure_debut,$Heure_fin,$Nom,$Prenom); // on retourne un tableau de 4 tableaux --> heure début, heure fin, nom patient, prenom patient (chaque ligne concerne un seul créneau)
+        return array($Heure_debut,$Heure_fin,$Nom,$Prenom); // super tableau dans lequel on a l'heure de debut et fin, le nom et prenom du patient pour chaque créneau
     }
     elseif ($job == "Admin")
     {
-
+        
     }
     elseif ($job == "Responsable")
     {
@@ -78,10 +81,24 @@ function get_creneaux($job, $ID, $connexion)
             $nom_intervention = $nom_intervention_array['Nom_intervention']; // on récupère le nom de l'intervention
             $request_HDebut = "SELECT Heure_debut FROM creneaux WHERE Nom_intervention = '$nom_intervention'"; // requête pour récupérer l'heure de début du créneau
             $request_HFin = "SELECT Heure_fin FROM creneaux WHERE Nom_intervention = '$nom_intervention'"; // requête pour récupérer l'heure de fin du créneau
-            $Heure_debut = do_request($connexion, $request_HDebut); // on effectue la requête
-            $Heure_fin = do_request($connexion, $request_HFin); // on effectue la requête
+            $request_IDp = "SELECT IDp FROM creneaux WHERE Nom_intervention = '$nom_intervention'";
+            $Heure_debut[] = do_request($connexion, $request_HDebut); // on effectue la requête
+            $Heure_fin[] = do_request($connexion, $request_HFin); // on effectue la requête
+            $IDp_super_array[] = do_request($connexion, $request_IDp);
         }
-        return array($Heure_debut,$Heure_fin);
+        foreach($IDp_super_array as $IDp_arrays)
+        {
+            foreach($IDp_arrays as $IDp_array)
+            {
+                $IDp = $IDp_array['IDp'];
+                $request_nom = "SELECT Nom FROM patient WHERE IDp ='$IDp'"; // requête pour récupérer les noms des patients
+                $request_prenom = "SELECT Prenom FROM patient WHERE IDp ='$IDp'"; // requête pour récupérer les prénoms des patients
+                $Nom_intervention[] = do_request($connexion, $request_intervention);
+                $Nom[] = do_request($connexion, $request_nom);
+                $Prenom[] = do_request($connexion, $request_prenom);
+            }
+        }
+        return array($Heure_debut,$Heure_fin,$Nom,$Prenom,$Nom_intervention); // super tableau dans lequel on a l'heure de debut et fin, le nom et prenom du patient et le type d'intervention pour chaque créneau
     }
 }
 
@@ -120,7 +137,6 @@ function print_creneaux($array) // pas d'utilité
     }
 }
 
-
 function generate_id($id, $nom, $prenom)
 {
     $first_letter = $prenom[0] ;
@@ -137,18 +153,40 @@ function generate_id($id, $nom, $prenom)
     return $login_def ;
 }
 
-
-
-
-function nom_jour($date) // fonction pour récupérer le jour de la date donné
+function nom_jour($date) // fonction pour récupérer le jour de la date donnée
 {
-   $jour_semaine = array(1=>"lundi", 2=>"mardi", 3=>"mercredi", 4=>"jeudi", 5=>"vendredi", 6=>"samedi", 7=>"dimanche");
+    $jour_semaine = array(1=>"lundi", 2=>"mardi", 3=>"mercredi", 4=>"jeudi", 5=>"vendredi", 6=>"samedi", 7=>"dimanche"); // tableau avec les jours de la semaine
+    list($jour, $mois, $annee) = explode ("/", $date);  // on récupère chaque élément de la date donnée
+    $timestamp = mktime(0,0,0, $mois, $jour, $annee); // on récupère le timestamp
+    $njour = date("N",$timestamp); // on récupère le numéro du jour de la semaine du timestamp
+    return $jour_semaine[$njour]; // on renvoie le jour correspondant au numéro du jour de la semaine
+}
 
-   list($annee, $mois, $jour) = explode ("/", $date);
-
-   $timestamp = mktime(0,0,0, date($mois), date($jour), date($annee));
-   $njour = date("N",$timestamp);
-
-   return $jour_semaine[$njour];
+// fonction pour récupérer les dates des jours de la semaine pour l'affichage de l'edt
+function get_dates_semaines($semaine) // argument = nb de semaines passées ou futures
+{
+    $semaine = $semaine * 7;
+    $jours_semaine = array(null, "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");    
+    $date = date('d/m/Y');
+    list($jour,$mois,$annee) = explode("/", $date);
+    $date_du_jour = date('d/m/y', mktime(0,0,0,$mois,$jour+$semaine,$annee));
+    $jour_de_date = ucfirst(nom_jour($date_du_jour));
+    for($k = 1; $k < sizeof($jours_semaine); $k++)
+    {
+        if ($jour_de_date == $jours_semaine[$k])
+        {
+            list($jour,$mois,$annee) = explode("/", $date_du_jour);
+            $days_back = $k+1;
+            // print($k);
+            $date_lundi = date("d/m/Y", mktime(0,0,0,$mois,$jour-$days_back,$annee));
+            // print($date_lundi);
+        }
+    }
+    for($d = 1; $d <= 8; $d++)
+    {
+        list($jour,$mois,$annee) = explode("/", $date_lundi);
+        $dates_semaine[] = date("d/m/Y", mktime(0,0,0,$mois,$jour+$d,$annee));
+    }
+    return $dates_semaine;
 }
 ?>
