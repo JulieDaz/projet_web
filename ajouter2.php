ed3cd29d7631de59f8d97fba2e0af344d990e3c6
JulieDaz <julie.dazeniere@gmail.com>
2017-11-28 21:27:41 +0100

    J : début ajout service acc et patho

--- ajouter2.php
+++ ajouter2.php
@@ -1,1 +1,25 @@
-coucou ici ajouter blib
+<?php 
+include("fonction.php"); 
+session_start() ;
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+     <title>Formulaire 1</title>
+</head>
+<body>
+
+<!-- Retourner au planning -->
+<form method="post" action="traitement.php"> 
+<input type="submit" value="Retourner au planning" name="retour_planning">
+</form>
+
+
+
+
+
+
+</body>
+</html>
