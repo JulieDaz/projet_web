f2f8f7717982df5669c69e04c1706dd1bdcae69c
Na_noouu <anais.vacq@gmail.com>
2017-12-06 10:16:22 +0100

    A:commit pour pull

--- demande_intervention2.php
+++ demande_intervention2.php
@@ -1,20 +1,4 @@
 <?php
-    $nomPatient = $_POST['Nom_patient'];
-    $prenomPatient = $_POST['Prenom_patient'] ;
-    $pathologie = $_POST['pathologie'] ;
-    $typeIntervention = $_POST['type_intervention'] ;
- ?>
-
-
-<html>
-  <title> Demande d'intervention 2/2 </title>
-  <?php
-    echo "M/Mme $nomPatient $prenomPatient a rdv pour un(e) $typeIntervention à cause de $pathologie" ;
-   ?>
-</html>
-
-<?php
-
 
 
 ?>
